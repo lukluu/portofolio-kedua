e2ae2e7442ca73fd47a42fb6a8bd01749b5c1aeb
lukman <laode.lukmana88@gmail.com>
2023-08-07 01:47:13 +0800

    membuat aplikasi portofolio dengan html, css, dan javascript kedua

---
+++ script.js
@@ -0,0 +1,150 @@
+// import "boxicons";
+
+// const header = document.querySelector("header");
+// // scroll event
+// window.addEventListener("scroll", () => {
+//   header.classList.toggle("sticky", window.scrollY > 100);
+// });
+
+// const navbar = document.querySelector(".navbar");
+// const menuIcon = document.querySelector("#icon-menu");
+// console.log(menuIcon);
+// menuIcon.addEventListener("click", () => {
+//   navbar.classList.toggle("active");
+//   // replace icon by atribut name ion-icon toggle
+//   if (menuIcon.getAttribute("name") === "close-outline") {
+//     menuIcon.setAttribute("name", "menu-outline");
+//     return;
+//   }
+//   menuIcon.setAttribute("name", "close-outline");
+// });
+
+const navLink = document.querySelectorAll("nav ul li a");
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    if (navLink[i].classList.contains("active")) {
+      for (let j = 0; j < navLink.length; j++) {
+        navLink[j].classList.remove("active");
+      }
+    } else {
+      for (let j = 0; j < navLink.length; j++) {
+        navLink[j].classList.remove("active");
+      }
+      navLink[i].classList.add("active");
+    }
+  });
+}
+
+let nav = document.querySelector("nav");
+console.log(nav);
+// scroll event
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("sticky", window.scrollY > 100);
+});
+
+let btnMenu = document.querySelector(".i");
+console.log(btnMenu);
+
+let navUl = document.querySelector("nav ul");
+btnMenu.addEventListener("click", () => {
+  navUl.classList.toggle("active");
+  nav.style.backgroundColor = "var(--bg-color)";
+  if (btnMenu.getAttribute("class") === "bx-menu") {
+    btnMenu.setAttribute("class", "bx-x");
+    return;
+  }
+  btnMenu.setAttribute("name", "bx-x");
+});
+
+// scroll event
+window.addEventListener("scroll", () => {
+  nav.classList.toggle("sticky", window.scrollY > 100);
+});
+const togelMode = document.querySelector(".mode-toggle");
+togelMode.addEventListener("click", () => {
+  document.body.classList.toggle("switch-mode");
+});
+
+// tablink
+
+function onTabClick(tabId) {
+  // Get all tab contents and tab links
+  const tabContents = document.querySelectorAll(".tab-content");
+  const tabLinks = document.querySelectorAll(".tab-link");
+
+  // Hide all tab contents
+  tabContents.forEach((content) => {
+    content.classList.remove("active-tab");
+  });
+
+  // Deactivate all tab links
+  tabLinks.forEach((link) => {
+    link.classList.remove("active-link");
+  });
+
+  // Show the selected tab content and activate the corresponding tab link
+  const selectedTab = document.getElementById(tabId);
+  const selectedLink = document.querySelector(`.tab-link[onclick="onTabClick('${tabId}')"]`);
+
+  if (selectedTab && selectedLink) {
+    selectedTab.classList.add("active-tab");
+    selectedLink.classList.add("active-link");
+  }
+}
+
+// type animasi
+const text = document.querySelector(".right");
+const textLoad = () => {
+  setTimeout(() => {
+    text.textContent = "Mahasiswa";
+  }, 0);
+  setTimeout(() => {
+    text.textContent = "Teknik";
+  }, 4000);
+  setTimeout(() => {
+    text.textContent = "Informatika";
+  }, 8000);
+};
+textLoad();
+setInterval(textLoad, 12000);
+
+// galeri
+const btnGaleri = document.querySelectorAll(".navigasi span");
+const galeri = document.querySelectorAll(".galeri div");
+// console.log(galeri);
+// console.log(btnGaleri);
+
+const filterImage = (image) => {
+  document.querySelector(".active-image").classList.remove("active-image");
+  image.target.classList.add("active-image");
+
+  galeri.forEach((galeri) => {
+    galeri.style.display = "none";
+    if (galeri.getAttribute("data-name") === image.target.getAttribute("data-name") || image.target.getAttribute("data-name") === "all") {
+      galeri.style.display = "block";
+    }
+  });
+};
+btnGaleri.forEach((span) => span.addEventListener("click", filterImage));
+
+// galeri onclick
+
+const show = document.querySelector(".show");
+const close = show.querySelector(".close");
+window.onload = function () {
+  for (let i = 0; i < galeri.length; i++) {
+    galeri[i].onclick = function () {
+      console.log(galeri[i]);
+      function showImage() {
+        let img = show.querySelector("img");
+        img.src = galeri[i].querySelector("img").src;
+        console.log(img.src);
+      }
+      showImage();
+      show.classList.add("active");
+      close.onclick = function () {
+        show.classList.remove("active");
+      };
+    };
+  }
+};
